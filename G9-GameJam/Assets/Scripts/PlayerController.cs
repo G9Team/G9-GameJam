a729488c260e8e1b04a81d4ae2c43a7658c79493
todokama <alehno01@mail.ru>
2022-12-10 02:06:56 +0300

    Add movement + camera

---
+++ G9-GameJam/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRigidbody;
+    private Animator playerAnimator;
+
+    [SerializeField] private float _speed = 5;
+    [SerializeField] private float _turnSpeed = 360;
+
+    private Vector3 _input;
+
+
+    void Start()
+    {
+        playerAnimator = GetComponent<Animator>();
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        GatherInput();
+    }
+
+    private void FixedUpdate()
+    {
+        Look();
+        Move();
+    }
+
+    private void GatherInput()
+    {
+        _input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        if (_input == Vector3.zero) playerAnimator.SetBool("isMoving", false);
+        else playerAnimator.SetBool("isMoving", true);
+    }
+
+    private void Look()
+    {
+        if (_input == Vector3.zero) return;
+
+        var rot = Quaternion.LookRotation(_input.ToIso(), Vector3.up);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, rot, _turnSpeed * Time.deltaTime);
+    }
+
+    private void Move()
+    {
+        playerRigidbody.MovePosition(transform.position + transform.forward * _input.normalized.magnitude * _speed * Time.deltaTime);
+    }
+}
+
+public static class Helpers
+{
+    private static Matrix4x4 _isoMatrix = Matrix4x4.Rotate(Quaternion.Euler(0, 45, 0));
+    public static Vector3 ToIso(this Vector3 input) => _isoMatrix.MultiplyPoint3x4(input);
+}
+
+
