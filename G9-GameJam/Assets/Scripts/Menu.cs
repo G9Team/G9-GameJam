79966c342730a1d5c2556dfbb30af6939899395b
todokama <alehno01@mail.ru>
2022-12-11 13:12:35 +0300

    Menu + UI +sound

---
+++ G9-GameJam/Assets/Scripts/Menu.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenuUI;
+    private static bool isGamePaused;
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && SceneManager.GetActiveScene().buildIndex !=0)
+        {
+            if (isGamePaused) Resume();
+            else Pause();
+        }
+    }
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isGamePaused = false;
+    }
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isGamePaused = true;
+    }
+
+    public void OpenNewScene(int id)
+    {
+        StartCoroutine(AsyncLoad(id));
+    }
+
+    private IEnumerator AsyncLoad(int index)
+    {
+        AsyncOperation ready = null;
+        ready = SceneManager.LoadSceneAsync(index);
+        while (!ready.isDone)
+        {
+            yield return null;
+        }
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
